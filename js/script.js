116a09cf4f1e198cdbc0ea6348a445dc2d5a9b22
AndreaGarofalo <andreasjobs94@outlook.it>
2022-12-15 15:36:39 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+console.log("JS OK");
+
+// Palidroma
+// Chiedere all’utente di inserire una parola tramiite un form
+// Creare una funzione per capire se la parola inserita è palindroma
+
+// 1 prendo gli elementi dal DOM
+// 2 raccolgo le risposte dell'utente in variabili
+// 3 creo una funzione in cui inverto la posizione delle lettere della parola
+// 4 confronto la parola prima e dopo l'inversione delle lettere
