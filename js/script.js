25d4ab19e58c1c7cb09c1b047217a93e5fd4a5a0
AndreaGarofalo <andreasjobs94@outlook.it>
2022-12-15 16:38:46 +0100

    created a funcion to get the user word

--- js/script.js
+++ js/script.js
@@ -5,12 +5,24 @@ console.log("JS OK");
 // Creare una funzione per capire se la parola inserita è palindroma
 
 // 1 prendo gli elementi dal DOM
-// 2 raccolgo le risposte dell'utente in variabili
-// 3 creo una funzione che prende il click del button
+// 2 creo una funzione che prende il click del button
+// 3 raccolgo le risposte dell'utente in variabili
 // 4 creo una funzione in cui inverto la posizione delle lettere della parola
 // 5 confronto la parola prima e dopo l'inversione delle lettere
 
 // 1 prendo gli elementi dal DOM
 const targetElement = document.getElementById("target");
-const word = document.getElementById("word");
+const form = document.getElementById("form");
+const wordField = document.getElementById("word");
 const button = document.getElementById("button");
+
+// 2 creo una funzione che prende il click del button
+
+form.addEventListener("submit", function (e) {
+  // blocco il comportamento naturale del form
+  e.preventDefault();
+
+  // 3 raccolgo le risposte dell'utente in variabili
+  const userWord = wordField.value.trim();
+  console.log(userWord);
+});
